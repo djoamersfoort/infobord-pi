019a80863530bf88641392a34bda96b6f8c715c4
Ronald Moesbergen <mail@rmoesbergen.nl>
2019-01-25 14:20:25 +0100

    Add xdotool, make x11login executable

--- stage-infobord/02-infobord/00-run.sh
+++ stage-infobord/02-infobord/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 install -v -o 1000 -g 1000 -m 0755 "files/infobord-controller.py" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
-install -v -o 1000 -g 1000 -m 0644 "files/x11login" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+install -v -o 1000 -g 1000 -m 0755 "files/x11login" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -v -o 1000 -g 1000 -m 0644 "files/.xinitrc" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -v -o 1000 -g 1000 -m 0644 "files/.profile" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 

38cce8864abf7de94a7e076c9af8758fd0e3dc2d
Ronald Moesbergen <djo@rmoesbergen.nl>
2019-01-25 14:36:23 +0100

    Add some instructions in build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 
 set -e
 
+# Do this before running this:
+#
+# apt install docker.io
+# Add your account to the docker group (usermod -aG docker <username>) and re-login
+# git submodule init
+# git submodule update
+
 sudo modprobe binfmt_misc
 cp -rv stage-infobord pi-gen/
 cp config pi-gen/
